9ef8649698bfe8bfda0d58403ee2dd7c72745c33
Valerie <vlrhntr@gmail.com>
2022-08-09 02:52:03 -0500

    Create main.py

---
+++ topic-6_fruitful_functions/main.py
@@ -0,0 +1,28 @@
+"""
+Unit Testing Code - Skeleton
+
+import sys
+
+def test(did_pass):
+    '''  Print the result of a test.  '''
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = "Test at line {0} FAILED.".format(linenum)
+    print(msg)
+
+
+def test_suite():
+    ''' Run the suite of tests for code in this module (this file).
+    '''
+
+    test(absolute_value(17) == 17)
+    test(absolute_value(-17) == 17)
+    test(absolute_value(0) == 0)
+    test(absolute_value(3.14) == 3.14)
+    test(absolute_value(-3.14) == 3.14)
+
+test_suite()        # Here is the call to run the tests
+
+"""
